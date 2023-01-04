1bafbb6c274f6a957963f46df9854f21d9a985a0
Grizzly182 <tiger.mananticot@yandex.ru>
2023-01-04 16:15:10 +0300

    refactored some stuff

--- Marseille/Assets/DBConnection/DBConnection.cs
+++ Marseille/Assets/DBConnection/DBConnection.cs
@@ -1,23 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
+﻿using MySql.Data.MySqlClient;
 
 namespace Marseille
 {
     public static class DBConnection
     {
-        private static readonly string server = "localhost";
-        private static readonly string database = "marseille";
-        private static readonly string user = "root";
-        private static readonly string password = "root";
-        private static readonly string port = "3306";
+        /// <summary>
+        /// Change "connectionString" variable to connect to other database.
+        /// </summary>
+        private static readonly string connectionString = "server=localhost; port=3306; user id=root; password=root; database=marseille";
 
-        private static readonly string connectionString = $"server={server};port={port};user id={user}; password={password}; database={database}";
-
-        private static MySqlConnection connection = new MySqlConnection(connectionString);
+        private static readonly MySqlConnection connection = new MySqlConnection(connectionString);
 
         public static void Open()
         {
