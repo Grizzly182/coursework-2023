aefebd032a6d36729a7cce8b7f66569df05a7ced
Grizzly182 <tiger.mananticot@yandex.ru>
2023-01-06 16:47:54 +0300

    Added Salt Generator and hash computer classes

---
+++ HotelDB/HashComputer.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography;
+
+namespace HotelDB
+{
+    /// <summary>
+    ///
+    /// </summary>
+    public class HashComputer
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="passwordWithSalt">Пароль с "насыпанной" солью</param>
+        /// <returns></returns>
+        public string GetPasswordHashAndSalt(string passwordWithSalt)
+        {
+            SHA256 sha = new SHA256CryptoServiceProvider();
+            byte[] dataBytes = Utility.GetBytes(passwordWithSalt);
+            byte[] resultBytes = sha.ComputeHash(dataBytes);
+
+            return Utility.GetString(resultBytes);
+        }
+    }
+}
---
+++ HotelDB/SaltGenerator.cs
@@ -0,0 +1,44 @@
+﻿using System.Security.Cryptography;
+
+namespace HotelDB
+{
+    /// <summary>
+    /// Класс, генерирующий "соль" для пароля пользователя.
+    /// </summary>
+    public static class SaltGenerator
+    {
+        /// <summary>
+        /// Поле в котором может содержаться криптографический генератор случайных чисел (RNGCryptoServiceProvider)
+        /// </summary>
+        private static RNGCryptoServiceProvider cryptoServiceProvider = null;
+
+        /// <summary>
+        /// Размер массива байтов
+        /// </summary>
+        private const int SALT_SIZE = 12; //при значении 12 итоговая длина хэша равна 16
+
+        /// <summary>
+        /// Конструктор, инициализирующий объект класса RNGCryptoServiceProvider
+        /// </summary>
+        static SaltGenerator()
+        {
+            cryptoServiceProvider = new RNGCryptoServiceProvider();
+        }
+
+        /// <summary>
+        /// Функция генерации случайной "соли".
+        /// </summary>
+        /// <returns>Строку случайно сгенерированного хэша.</returns>
+        public static string GetSaltString()
+        {
+            //Создаём массив байтов
+            byte[] saltBytes = new byte[SALT_SIZE];
+
+            //Генерируем в массив случайные не нулевые значения
+            cryptoServiceProvider.GetNonZeroBytes(saltBytes);
+
+            //Воозвращаем строку получившегося хэша заданного размера
+            return Utility.GetString(saltBytes);
+        }
+    }
+}
