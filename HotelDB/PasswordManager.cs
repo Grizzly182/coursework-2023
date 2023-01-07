afbeee5df7be5d8797fae58c1c796a121a2cf8f8
Grizzly182 <tiger.mananticot@yandex.ru>
2023-01-07 21:17:36 +0300

    Created class PasswordManager

---
+++ HotelDB/PasswordManager.cs
@@ -0,0 +1,24 @@
+﻿namespace HotelDB
+{
+    /// <summary>
+    /// Выполняет все функции, связанные с изменением,
+    /// созданием и хэшированием паролей.
+    /// </summary>
+    public class PasswordManager
+    {
+        private readonly HashComputer hashComputer = new HashComputer();
+
+        public string GeneratePasswordHash(string password, out string salt)
+        {
+            salt = SaltGenerator.GenerateSaltString();
+            string finalString = password + salt;
+            return hashComputer.GetPasswordHashAndSalt(finalString);
+        }
+
+        public bool IsPasswordsMatch(string password, string salt, string secondPasswordHash)
+        {
+            string firstPassword = password + salt;
+            return secondPasswordHash == hashComputer.GetPasswordHashAndSalt(firstPassword);
+        }
+    }
+}
