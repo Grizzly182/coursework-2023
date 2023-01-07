65e97247ee8ebe0040789ec30c9b67f89fd2b606
Grizzly182 <tiger.mananticot@yandex.ru>
2023-01-07 21:18:29 +0300

    Commented whole class

--- HotelDB/HashComputer.cs
+++ HotelDB/HashComputer.cs
@@ -3,15 +3,17 @@
 namespace HotelDB
 {
     /// <summary>
-    ///
+    /// Класс, служащий для вычисления хэша.
     /// </summary>
     public class HashComputer
     {
         /// <summary>
-        ///
+        /// Генерирует SHA256 хэш из строки (аргумента функции) с помощью
+        /// функции ComputeHash(byte[] buffer).
         /// </summary>
         /// <param name="passwordWithSalt">Пароль с "насыпанной" солью</param>
-        /// <returns></returns>
+        /// <returns>Возвращает строку хэша пароля </returns>
+        /// <remarks>В качестве аргумента необходимо передавать строку пароля + строка соли</remarks>
         public string GetPasswordHashAndSalt(string passwordWithSalt)
         {
             SHA256 sha = new SHA256CryptoServiceProvider();
