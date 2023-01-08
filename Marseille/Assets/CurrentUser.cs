3d4132c80da36d29da2bfc728121fd0be9709408
Grizzly182 <tiger.mananticot@yandex.ru>
2023-01-08 11:30:58 +0300

    Created CurrentUser static class

---
+++ Marseille/Assets/CurrentUser.cs
@@ -0,0 +1,20 @@
+﻿namespace Marseille
+{
+    internal static class CurrentUser
+    {
+        public static string Login { get; private set; }
+        public static string Name { get; private set; }
+        public static string Surname { get; private set; }
+        public static string Patronymic { get; private set; }
+        public static Role UserRole { get; private set; }
+
+        public static void SetLoggedUser(string login, string name, string surname, string patronymic, int role)
+        {
+            Login = login;
+            Name = name;
+            Surname = surname;
+            Patronymic = patronymic;
+            UserRole = (Role)role;
+        }
+    }
+}
