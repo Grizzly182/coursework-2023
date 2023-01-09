05e131c0bbcd8728771aa09d2031a9664c6b8763
Grizzly182 <tiger.mananticot@yandex.ru>
2023-01-09 22:42:13 +0300

    moved here

---
+++ Marseille/Exceptions/WrongLoginOrPasswordException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Marseille
+{
+    /// <summary>
+    ///
+    /// </summary>
+    internal class WrongLoginOrPasswordException : Exception
+    {
+        public override string Message => "Неверный логин или пароль";
+    }
+}
