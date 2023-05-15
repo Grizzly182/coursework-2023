f4868f501f3aeb2e0ec809685387385e37cd7621
Grizzly182 <tiger.mananticot@yandex.ru>
2023-05-15 14:27:32 +0300

    Created utility class

---
+++ Marseille/Assets/Utility.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marseille.Assets
+{
+    public class Utility
+    {
+        /// <summary>
+        /// Проверяет всю строку на наличие букв, отличающихся от английского алфавита.
+        /// </summary>
+        /// <param name="text">Строка передаваемая на проверку.</param>
+        /// <returns>Возвращает <see langword="true"/> если в строке все буквы
+        /// являются частью английского алфавита, иначе возвращает <see langword="false"/></returns>
+        public static bool IsEnglishText(string text)
+        {
+            foreach (char ch in text.ToLower())
+            {
+                if (!(ch >= 97 && ch <= 122 || char.IsDigit(ch)))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
