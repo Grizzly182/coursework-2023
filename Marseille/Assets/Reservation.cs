dddd63f2c2b269c82b3e386a95c0aed3b1737ff6
Grizzly182 <tiger.mananticot@yandex.ru>
2023-05-15 14:25:56 +0300

    Classes for reservations

---
+++ Marseille/Assets/Reservation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marseille
+{
+    public class Reservation
+    {
+        public uint Id { get; private set; }
+        public DateTime CreateTime { get; private set; }
+        public DateTime CheckInDate { get; private set; }
+        public DateTime CheckOutDate { get; private set; }
+        public Room Room { get; private set; }
+        public List<Client> Clients { get; private set; }
+
+        public Reservation(uint id, DateTime createTime, DateTime checkInDate, DateTime checkOutDate, Room room)
+        {
+            Id = id;
+            CreateTime = createTime;
+            CheckInDate = checkInDate;
+            CheckOutDate = checkOutDate;
+            Room = room;
+        }
+    }
+}
