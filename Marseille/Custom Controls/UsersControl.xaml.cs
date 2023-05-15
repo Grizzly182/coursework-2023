6b25c1ac6737da46ebcf9395f9c6323ab55148f7
Grizzly182 <tiger.mananticot@yandex.ru>
2023-05-15 14:24:37 +0300

    Custom controls for Users and Rooms

---
+++ Marseille/Custom Controls/UsersControl.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows.Controls;
+
+namespace Marseille.Custom_Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для UsersControl.xaml
+    /// </summary>
+    public partial class UsersControl : UserControl
+    {
+        private User _user;
+
+        public User User
+        {
+            get => _user;
+            private set => _user = value;
+        }
+
+        public bool IsCurrentUser
+        { get { return this.User.Id != User.CurrentUser.Id; } }
+
+        public UsersControl()
+        {
+            InitializeComponent();
+        }
+
+        public UsersControl(User user)
+        {
+            _user = user;
+            InitializeComponent();
+        }
+    }
+}
