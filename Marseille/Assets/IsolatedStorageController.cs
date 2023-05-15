d45881bd09fbe0acd8393f371813667c9245fa00
Grizzly182 <tiger.mananticot@yandex.ru>
2023-05-15 14:20:07 +0300

    implemented "remember me" feature

---
+++ Marseille/Assets/IsolatedStorageController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Windows.Input;
+
+namespace Marseille.Assets
+{
+    internal class IsolatedStorageController
+    {
+        private const string _defaultIsolatedStorageFilePath = "storage";
+
+        public static bool IsolatedStorageEnabled { get; set; }
+
+        public static void Save(Dictionary<string, string> values)
+        {
+            IsolatedStorageFile storage = IsolatedStorageFile.GetUserStoreForDomain();
+
+            try
+            {
+                IsolatedStorageFileStream stream = new IsolatedStorageFileStream(_defaultIsolatedStorageFilePath, FileMode.OpenOrCreate, storage);
+                using (StreamWriter writer = new StreamWriter(stream))
+                {
+                    foreach (var item in values)
+                    {
+                        writer.WriteLineAsync($"{item.Key}:{item.Value}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorMessagesProider.ShowError("Возникла непредвиденная ошибка: " + ex.Message);
+            }
+        }
+
+        // TODO: Comment this later
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static string Read(string key)
+        {
+            IsolatedStorageFile storage = IsolatedStorageFile.GetUserStoreForDomain();
+
+            try
+            {
+                IsolatedStorageFileStream stream = new IsolatedStorageFileStream(_defaultIsolatedStorageFilePath, FileMode.OpenOrCreate, storage);
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        string[] keyAndValue = reader.ReadLine().Split(':');
+
+                        if (keyAndValue[0].Contains(key)) // keyAndValue[0] is key and [1] is a value of the key
+                        {
+                            return keyAndValue[1];
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorMessagesProider.ShowError("Возникла непредвиденная ошибка: " + ex.Message);
+            }
+            return null;
+        }
+
+        public static void Remove()
+        {
+            if (IsolatedStorageEnabled)
+            {
+            }
+        }
+
+        public static void Clear()
+        {
+            IsolatedStorageFile storage = IsolatedStorageFile.GetUserStoreForDomain();
+            try
+            {
+                IsolatedStorageFileStream stream = new IsolatedStorageFileStream(_defaultIsolatedStorageFilePath, FileMode.Truncate, storage);
+                using (StreamWriter writer = new StreamWriter(stream))
+                {
+                    writer.WriteLineAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorMessagesProider.ShowError("Возникла непредвиденная ошибка: " + ex.Message);
+            }
+        }
+    }
+}
--- Marseille/Forms/LoginWindow.xaml.cs
+++ Marseille/Forms/LoginWindow.xaml.cs
@@ -4,6 +4,9 @@ using System.Windows.Media;
 using HotelDB;
 using System.Windows.Input;
 using Marseille.Assets;
+using System.Collections.Generic;
+
+using Marseille.Assets;
 
 namespace Marseille.Forms
 {
@@ -14,8 +17,43 @@ namespace Marseille.Forms
     {
         public LoginWindow()
         {
+            if (IsolatedStorageController.Read("login") != null)
+            {
+                try
+                {
+                    string login = IsolatedStorageController.Read("login");
+                    string password = IsolatedStorageController.Read("password");
+
+                    if (DBConnection.Login(login, password))
+                    {
+                        DBConnection.GetUserData(login, out uint id, out string name, out string surname, out string patronymic, out int role);
+                        User.SetLoggedUser(id, login, name, surname, patronymic, (Role)role);
+
+                        switch (User.CurrentUser.UserRole)
+                        {
+                            case Role.Admin:
+                                AdminWindow adminWindow = new AdminWindow();
+                                Hide();
+                                adminWindow.Show();
+
+                                return;
+
+                            case Role.Manager:
+                                ManagerWindow managerWindow = new ManagerWindow();
+                                Hide();
+                                managerWindow.Show();
+
+                                return;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ErrorMessagesProider.ShowError("Ошибка при входе.\n" + ex.Message);
+                }
+            }
+
             InitializeComponent();
-            //loginButton.BorderBrush = new SolidColorBrush(Colors.Transparent);
             loginBox.CommandBindings.Add(new CommandBinding(ApplicationCommands.Paste, OnPaste));
             passwordBox.CommandBindings.Add(new CommandBinding(ApplicationCommands.Paste, OnPaste));
         }
@@ -33,25 +71,51 @@ namespace Marseille.Forms
         {
             string login = loginBox.Text;
             string password = passwordBox.Password;
-            if (DBConnection.Login(login, password))
+
+            try
             {
-                try
+                if (DBConnection.Login(login, password))
                 {
-                    DBConnection.GetUserData(login, out string name, out string surname, out string patronymic, out int role);
-                    User.SetLoggedUser(login, name, surname, patronymic, (Role)role);
-                    WindowsController.OpenWindow(this, new AdminWindow());
-                    MessageBox.Show(User.Login + User.Name + User.Patronymic + User.Surname + User.UserRole.ToString());
+                    if ((bool)rememberMeCheckBox.IsChecked)
+                    {
+                        IsolatedStorageController.IsolatedStorageEnabled = true;
+
+                        IsolatedStorageController.Save(new Dictionary<string, string>(){
+                            { "login", login},
+                            { "password", password }
+                        });
+                    }
+
+                    DBConnection.GetUserData(login, out uint id, out string name, out string surname, out string patronymic, out int role);
+                    User.SetLoggedUser(id, login, name, surname, patronymic, (Role)role);
+
+                    switch (User.CurrentUser.UserRole)
+                    {
+                        case Role.Admin:
+                            AdminWindow adminWindow = new AdminWindow();
+                            Hide();
+                            adminWindow.Show();
+                            break;
+
+                        case Role.Manager:
+                            ManagerWindow managerWindow = new ManagerWindow();
+                            Hide();
+                            managerWindow.Show();
+                            break;
+
+                        default: break;
+                    }
                 }
-                catch (Exception ex)
+                else
                 {
-                    ErrorMessagesProider.ShowError("Произошла ошибка при входе\n" + ex.Message);
+                    loginBox.Clear();
+                    passwordBox.Clear();
+                    ErrorMessagesProider.ShowError("Неверные логин или пароль.");
                 }
             }
-            else
+            catch (Exception ex)
             {
-                loginBox.Clear();
-                passwordBox.Clear();
-                ErrorMessagesProider.ShowError("Неверные логин или пароль.");
+                ErrorMessagesProider.ShowError("Произошла ошибка при входе\n" + ex.Message);
             }
         }
 
@@ -62,7 +126,7 @@ namespace Marseille.Forms
 
         private void loginBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
-            if (!IsEnglishText(e.Text))
+            if (!Assets.Utility.IsEnglishText(e.Text))
             {
                 e.Handled = true;
                 return;
@@ -71,29 +135,11 @@ namespace Marseille.Forms
 
         private void passwordBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
-            if (!IsEnglishText(e.Text))
+            if (!Assets.Utility.IsEnglishText(e.Text))
             {
                 e.Handled = true;
                 return;
             }
-        }
-
-        /// <summary>
-        /// Проверяет всю строку на наличие букв, отличающихся от английского алфавита.
-        /// </summary>
-        /// <param name="text">Строка передаваемая на проверку.</param>
-        /// <returns>Возвращает <see langword="true"/> если в строке все буквы
-        /// являются частью английского алфавита, иначе возвращает <see langword="false"/></returns>
-        private bool IsEnglishText(string text)
-        {
-            foreach (char ch in text.ToLower())
-            {
-                if (!(ch >= 97 && ch <= 122 || char.IsDigit(ch)))
-                {
-                    return false;
-                }
-            }
-            return true;
         }
 
         private void passwordBox_PasswordChanged(object sender, RoutedEventArgs e)
